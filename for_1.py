710388ed8c067b217c9b88ed2d60207725a0574c
azizbekkholdaraliyev <azizbekholdaraliyev@gmail.com>
2026-06-25 22:29:39 +0500

    Create for_1.py

---
+++ for_1.py
@@ -0,0 +1,18 @@
+# Kamida 5 elementdan iborat ismlar degan ro'yxat tuzing, 
+# va ro'yxatdagi har bir ismga takrorlanuvchi xabar yozing
+
+# Yuoqirdagi tsikl tugaganidan so'ng, ekranga "Kod n marta takrorlandi" degan xabarni chiqaring 
+# (n o'rniga kod necha marta takrorlanganini yozing)
+
+#natija shunday chiqishi kerak:
+#salom Ali
+#salom Vali
+#salom Hasan
+#salom Husan
+#salom Olim
+#kod 5 marta takrorlandi
+
+ismlar = ['Ali','Vali','Hasan','Husan','Olim']
+for ism in ismlar:
+    print(f"Assalom alaykum, {ism}. Sahifamizga xush kelibsiz!")
+print(f"Kod {len(ismlar)} marta takrorlandi")
