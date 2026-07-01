75dae72c2b77fe26c97f443cfb15a255480501ae
azizbekkholdaraliyev <azizbekholdaraliyev@gmail.com>
2026-07-01 23:51:38 +0500

    Create dict_2.py

---
+++ dict_2.py
@@ -0,0 +1,14 @@
+# Oila a'zolaringizning sevimli taomlari lug'atini tuzing. Lug'atda kamida 5 ta ism-taom jufltgii bo'lsin.
+# Kamida uch kishining sevimli taomini konsolga chiqaring: Alining sevimli taomi osh
+fav_dish = {
+    'ayam':'makron',
+    'buvim':'shurva',
+    'akam':'osh',
+    'amakim':'mastava',
+    'ammam':'dimlama'
+}
+print(f"ayamning sevimli taomi {fav_dish['ayam']}")
+print(f"buvimning sevimli taomi {fav_dish['buvim']}")
+print(f"akamning sevimli taomi {fav_dish['akam']}")
+print(f"amakimning sevimli taomi {fav_dish['amakim']}")
+print(f"ammamning sevimli taomi {fav_dish['ammam']}")
