21bcbc93efe4798583becb7fd1ba4430995216e5
azizbekkholdaraliyev <azizbekholdaraliyev@gmail.com>
2026-07-01 23:52:44 +0500

    Create for_2.py

---
+++ for_2.py
@@ -0,0 +1,5 @@
+# 10 dan 100 gacha bo'lgan toq sonlar ro'yxatini tuzing. 
+# Ro'yxatning xar bir elementining kubini yangi qatordan konsolga chiqaring.
+sonlar = []
+for sonlar in range(11,101,2):
+    print(sonlar)
