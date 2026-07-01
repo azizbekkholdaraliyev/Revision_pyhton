415437a72b85b2b85c441c9868390c29486cfa86
azizbekkholdaraliyev <azizbekholdaraliyev@gmail.com>
2026-07-01 23:51:29 +0500

    Create dict_1.py

---
+++ dict_1.py
@@ -0,0 +1,64 @@
+# otam (onam, akam, ukam, va hokazo) degan lug'at yarating va lug'atga shu inson haqida
+# kamida 3 ta m'alumot kiriting (ismi, tu'gilgan yili, shahri, manzili va hokazo). 
+# Lug'atdagi ma'lumotni matn shaklida konsolga chiqaring :Otamning ismi Mavlutdin, 1954-yilda, Samarqand viloyatida tug'ilgan
+
+otam = {
+    'ismi':'ali',
+    'yili':1978,
+    'shahri':'Fergana',
+    'kasb':'haydovchi'
+}
+yili = otam['yili']
+shahri = otam['shahri']
+kasb = otam['kasb']
+print(
+    f"{otam['ismi'].title()}, {yili}-yil, {shahri}da tug'ilgan, kasbi {kasb}"
+)
+onam = {
+    'ismi':'mohira',
+    'yili':1984,
+    'shahri':'Fergana',
+    'kasb':'bugalter'
+}
+yili = onam['yili']
+shahri = onam['shahri']
+kasb = onam['kasb']
+print(
+    f"{onam['ismi'].title()}, {yili}-yil, {shahri}da tug'ilgan, kasbi {kasb}"
+)
+akam1 = {
+    'ismi':'yaxyobek',
+    'yili':2006,
+    'shahri':'Fergana',
+    'kasb':'usta'
+}
+yili = akam1['yili']
+shahri = akam1['shahri']
+kasb = akam1['kasb']
+print(
+    f"{akam1['ismi'].title()}, {yili}-yil, {shahri}da tug'ilgan, kasbi {kasb}"
+)
+akam2 = {
+    'ismi':'kamolbek',
+    'yili':2003,
+    'shahri':'Fergana',
+    'kasb':'haydovchi'
+}
+yili = akam2['yili']
+shahri = akam2['shahri']
+kasb = akam2['kasb']
+print(
+    f"{akam2['ismi'].title()}, {yili}-yil, {shahri}da tug'ilgan, kasbi {kasb}"
+)
+buvim = {
+    'ismi':'muborak',
+    'yili':1954,
+    'shahri':'Fergana',
+    'kasb':'tikuvchi'
+}
+yili = buvim['yili']
+shahri = buvim['shahri']
+kasb = buvim['kasb']
+print(
+    f"{buvim['ismi'].title()}, {yili}-yil, {shahri}da tug'ilgan, kasbi {kasb}"
+)
