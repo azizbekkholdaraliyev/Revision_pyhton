65194aba2bcf903c7f12a50f9a42dc144542a029
azizbekkholdaraliyev <azizbekholdaraliyev@gmail.com>
2026-07-02 23:45:37 +0500

    Create if_1.py

---
+++ if_1.py
@@ -0,0 +1,3 @@
+#mijoz bir mashina aytishi kerak: nomi, rangi, ishlab chiqarilgan sanasini aytadi va bu dastur 
+#mijoz tariflab bergan mashinani avto salonda bormi yoqmi aniqlab beradi
+cars = ['malibu 1', 'malibu 2', 'nexia 1', 'nexia 2', ]
