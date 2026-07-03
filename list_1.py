8eb155b32c36b6a3183de22b0b1d6c03a626c008
azizbekkholdaraliyev <azizbekholdaraliyev@gmail.com>
2026-07-03 10:31:13 +0500

    Create list_1.py

---
+++ list_1.py
@@ -0,0 +1,7 @@
+# ismlar degan ro'yxat yarating va kamida 3 ta yaqin do'stingizning ismini kiriting
+# Ro'yxatdagi har bir do'stingizga qisqa xabar yozib konsolga chiqaring: 
+
+ismlar = ['samandar', 'yaxyobek', 'firdavs']
+print(f"salom {ismlar[0].capitalize()}, qalesz jigarm")
+print(f"{ismlar[1].title()} nima gap")
+print(f"{ismlar[2].upper()} boshmisan")
