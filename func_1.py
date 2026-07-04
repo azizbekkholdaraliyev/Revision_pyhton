f40e9ee90c3fa37084571a84eed7f4277bfdb03d
azizbekkholdaraliyev <azizbekholdaraliyev@gmail.com>
2026-07-04 17:32:46 +0500

    Create func_1.py

---
+++ func_1.py
@@ -0,0 +1,7 @@
+# Foydalanuvchi ismi va yoshini so'rab, uning tug'ilgan yilini hisoblaydigan funksiya yozing.
+def ism_yil(ism, yosh):
+    """foydalanuvchidan ism va yoshini sorab, 
+    unga tugilgan yilini ismi bilan murojat qilib aytuvchi dastur"""
+    print(f"salom {ism.title()}, siz {2026-yosh}-yida tugildansiz")
+
+ism_yil('anvar', 17)
