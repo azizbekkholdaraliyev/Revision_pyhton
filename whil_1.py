256921d2cafcb36c5bc14f98c619adedbda933bd
azizbekkholdaraliyev <azizbekholdaraliyev@gmail.com>
2026-07-05 16:39:49 +0500

    Create whil_1.py

---
+++ whil_1.py
@@ -0,0 +1,10 @@
+# Foydalanuvchidan yaxshi ko'rgan kitoblarini kiritishni so'rang. 
+# Foydalanuvchi stop so'zini yozishi bilan dasturni to'xtating
+
+sora = 'yaxshi korgan kitobingni kirit: '
+while True:
+    qiymat = input(sora)
+    if qiymat == 'stop':
+        break
+    else:
+        continue
