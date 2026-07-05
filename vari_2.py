f9950ac590e4bd68bd3cbbde9348636ac4353229
azizbekkholdaraliyev <azizbekholdaraliyev@gmail.com>
2026-07-05 22:30:04 +0500

    Create vari_2.py

---
+++ vari_2.py
@@ -0,0 +1,17 @@
+# # 5 ning 4-darajasini toping
+# print(5**4)
+
+# # 22 ni 4 ga bo'lganda qancha qoldiq qoladi?
+# print(22%4)
+
+# # Tomonlari 125 ga teng kvadratning yuzi va perimetrini toping
+# print(125+125+125)
+# print((125+125)*2/2)
+
+# # Diametri 12 ga teng bo'lgan doiraning yuzini toping  
+# # π=3.14
+# D = 12      
+# pi = 3.14   
+# r = D / 2
+# S = pi * (r ** 2) 
+# print("Doiraning yuzi:", S)
