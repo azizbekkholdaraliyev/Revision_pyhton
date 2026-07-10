dda19e28ed8a8928d2170b56b5875e452b385fa6
azizbekkholdaraliyev <azizbekholdaraliyev@gmail.com>
2026-07-10 22:55:14 +0500

    Create oop_1.py

---
+++ oop_1.py
@@ -0,0 +1,9 @@
+# Talaba klassiga yana bir, fanlar degan xususiyat qo'shing. Bu xususiyat parametr sifatida uzatilmasin va obyekt yaratilganida bo'sh ro'yxatdan iborat bo'lsin (self.fanlar=[])
+# Fan degan yangi klass yarating va bu klassdan turli fanlar uchun alohida obyektlar yarating.
+# Talaba klassiga fanga_yozil() degan yangi metod yozing. Bu metod parametr sifatida Fan klassiga tegishli obyektlarni qabul qilib olsin va talabaning fanlar ro'yxatiga qo'shib qo'ysin.
+# Talabaning fanlari ro'yxatidan biror fanni o'chirib tashlash uchun remove_fan() metodini yozing. Agar bu metodga ro'yxatda yo'q fan uzatilsa "Siz bu fanga yozilmagansiz" xabarini qaytarsin.
+# Yuqoridagi Shaxs klassidan boshqa turli voris klasslar yaratib ko'ring (masalan Professor, Foydalanuvchi, Sotuvchi, Mijoz va hokazo)
+# Har bir klassga o'ziga hoz xususiyatlar va metodlar yuklang.
+# Barcha yangi klasslar uchun get_info() metodini qayta yozib chiqing.
+# Voris klasslardan yana boshqa voris klass yaratib ko'ring. Misol uchun Foydalanuvchi klassidan Admin klassini yarating.
+# Admin klassiga foydalanuvchida yo'q yangi metodlar yozing, masalan, ban_user() metodi konsolga "Foydalanuvchi bloklandi" degan matn chiqarsin.
